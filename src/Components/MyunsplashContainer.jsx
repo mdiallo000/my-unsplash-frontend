cfb272bc31d2b77bfdf57ff5550862f275cf0625
mdiallo98 <mamadou1237@gmail.com>
2022-03-22 05:46:53 -0400

    useState

--- src/Components/MyunsplashContainer.jsx
+++ src/Components/MyunsplashContainer.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function MyunsplashContainer() {
+  const [data, Setdata] = useState('');
+
+  useEffect(() => {
+    async function fetchMyApi() {
+      let data = await fetch('http://localhost:1337/api/unsplashes');
+
+      let response = await data.json();
+      Setdata(response);
+    }
+  }, [data]);
+
   return (
     <div>
       <h1 style={{ textAlling: 'center' }}>My unsplash</h1>
