5074c8cb5b792b001f455f9e2092f3d00689e151
mdiallo98 <mamadou1237@gmail.com>
2022-03-22 05:44:27 -0400

    pratice api

---
+++ src/Components/MyunsplashContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyunsplashContainer() {
+  return (
+    <div>
+      <h1 style={{ textAlling: 'center' }}>My unsplash</h1>
+    </div>
+  );
+}
+
+export default MyunsplashContainer;
