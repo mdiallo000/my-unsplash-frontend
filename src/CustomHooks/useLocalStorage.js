692d037844c75c42b0347fa17d08ee1f4ef407e7
mdiallo98 <mamadou1237@gmail.com>
2022-03-25 07:15:27 -0400

    create local storage hook finished

--- src/CustomHooks/useLocalStorage.js
+++ src/CustomHooks/useLocalStorage.js
@@ -10,4 +10,9 @@ export const useLocaStorage = (key, storedValues) => {
   const [value, setvalue] = useState(() =>
     getLocalStorageItems(key, storedValues)
   );
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, storedValues]);
+
+  return [value, setvalue];
 };
