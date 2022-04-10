a8b538ae3cff5521ce0e6dccf8432be9b176060d
mdiallo98 <mamadou1237@gmail.com>
2022-04-10 03:02:28 -0400

    change dummy data

--- src/Components/MyUnsplash.jsx
+++ src/Components/MyUnsplash.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function MyUnsplash({ dataValues }) {
+  console.log(typeof data);
   return (
     <div>
       <h1>My UnSplash</h1>
       <div>
         {dataValues.map((val) => {
-          return <h1>{val.title}</h1>;
+          return (
+            <div>
+              <h1>{val.attributes.title}</h1>
+              <img src={val.attributes.img.data.attributes.formats.medium} />
+            </div>
+          );
         })}
       </div>
     </div>
--- src/Components/MyunsplashContainer.jsx
+++ src/Components/MyunsplashContainer.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 import MyUnsplash from './MyUnsplash';
 import useFetch from '../CustomHooks/useFetch';
 function MyunsplashContainer() {
-  const myApiUrl = ` http://localhost:1339/api/unsplashes/`;
+  const myApiUrl = 'http://localhost:1339/api/unsplashes?populate=*';
   const [data, ApiError, loadingStatus] = useFetch(myApiUrl);
 
   if (loadingStatus) {
@@ -11,6 +11,7 @@ function MyunsplashContainer() {
   if (ApiError) {
     return <h1>There was an Error</h1>;
   } else {
+    console.log(typeof data);
     return <MyUnsplash dataValues={data} />;
   }
 }
